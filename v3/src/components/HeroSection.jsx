afbbd58e22aff1942fd7551ff5b32877f8ab16bf
Bellmin <whdals011203@naver.com>
2025-07-12 23:16:59 +0900

    refactor: 이미지 base url 수정

--- v3/src/components/HeroSection.jsx
+++ v3/src/components/HeroSection.jsx
@@ -10,21 +10,21 @@ export default function HeroSection({ onContactClick }) {
         </p>
         <div className="social">
           <button onClick={onContactClick} className="social-button">
-            <img fetchPriority="high" src="static/button.svg" alt="Contact" />
+            <img fetchPriority="high" src={`${import.meta.env.BASE_URL}/static/button.svg`} alt="Contact" />
           </button>
           <button onClick={()=>window.open("https://velog.io/@rlajm1203")} className="social-button">
-            <img fetchPriority="high" src="static/velog.svg" alt="Contact" />
+            <img fetchPriority="high" src={`${import.meta.env.BASE_URL}/static/velog.svg`} alt="Contact" />
           </button>
           <button onClick={()=>window.open("")} className="social-button">
-            <img fetchPriority="high" src="static/linkedIn.svg" alt="Contact" />
+            <img fetchPriority="high" src={`${import.meta.env.BASE_URL}/static/linkedIn.svg`} alt="Contact" />
           </button>
           <button onClick={()=>window.open("https://github.com/rlajm1203")} className="social-button">
-            <img fetchPriority="high" src="static/github.svg" alt="Contact" />
+            <img fetchPriority="high" src={`${import.meta.env.BASE_URL}/static/github.svg`} alt="Contact" />
           </button>
         </div>
       </div>
       <div className="body-right-img">
-        <img fetchPriority="high" src="static/body-img.png" alt="" />
+        <img fetchPriority="high" src="profile/static/body-img.png" alt="" />
       </div>
     </section>
   );
