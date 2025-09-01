14c64583103847009f22056a5d49274199ae1a82
Bellmin <whdals011203@naver.com>
2025-09-01 15:16:15 +0900

    refactor: project 목록 순서 변경

--- v3.1/src/portfolio/project/constants/projects.js
+++ v3.1/src/portfolio/project/constants/projects.js
@@ -3,35 +3,42 @@
 // TODO: Project detail description 채우기
 export const PROJECTS = [
   {
-    id: 3,
-    title: "EEOS",
-    semiTitle: "동아리 행사 관리 시스템",
-    detail: `Econovation Event Operating System 의 약자로
-            에코노베이션 동아리의 행사 관리 및 출석 체크 시스템입니다.`,
-    imageSrc: "/static/project/eeos.svg",
-    year: "2024.03 ~ NOW",
+    id: 1,
+    title: "Whoz-In",
+    semiTitle: "wi-fi 기반 동아리방 재실 확인 시스템",
+    detail: `Wi-Fi 기술을 기반으로 하는
+            동아리방 재실 인원 현황 확인 서비스 입니다.
+    `,
+    imageSrc: "/static/project/whozIn.svg",
+    year: "2024.09 ~ 2025.03",
     role: "back-end developer",
-    description: `1. 짧은 기간 내 코드베이스를 빠르게 이해하여 프로젝트 전반 구조를 파악하여 업무 적응
-                  2. Redis Delayed Queue를 활용한 출석체크 예약기능 구현
-                  3. Tree 구조를 활용한, 댓글/답글 구조 모델링`,
-    viewLink: "https://www.eeos.econovation.kr",    // TODO: 실제 프로젝트 URL로 교체
-    githubLink: "https://github.com/JNU-econovation/EEOS-BE",  // TODO: 실제 GitHub 레포로 교체
+    description: `1. mDNS, ARP 프로토콜을 활용한 동아리방 재실 현황 알고리즘 설계 및 구현
+                  2. Let's Encrypt를 활용한 SSL 적용으로 인한 AWS 서버 비용 감축
+                  3. Spring Security 도입 및 Filter Chain 설정 고급화
+                  4. 가독성과 유지보수를 위한 클린 아키텍처 및 CQRS 적용
+                  5. 도메인 로직 보호를 위한 도메인 중심 설계 적용`,
+    viewLink: "#",
+    githubLink: "https://github.com/JNU-econovation/Whoz-In-BE",
     detailDescription: [
       {
-        title: `짧은 기간 내 코드베이스를 빠르게 이해하여 프로젝트 전반 구조를 파악하여 업무 적응`,
+        title: `mDNS, ARP 프로토콜을 활용한 동아리방 재실 현황 알고리즘 설계 및 구현`,
         description: `테스트 입니다.`
       },
       {
-        title: `Redis Delayed Queue를 활용한 출석체크 예약기능 구현`,
-        description: `Redis Sorted Set 자료구조를 사용하여, 각 행사가 종료되는 Timestamp 값을 Score로 사용하였습니다.
-                      
-                      다른 도메인에서도 특정 Task를 예약할 수 있도록 Delayed Queue 클래스를 정의하여 추상화 하였습니다.`,
-        img : "url"
+        title: `Let's Encrypt를 활용한 SSL 적용으로 인한 AWS 서버 비용 감축`,
+        description: `테스트 입니다.`
       },
       {
-        title: `Tree 구조를 활용한, 댓글/답글 구조 모델링`,
-        description: `행사 조회 페이지에서, 댓글과 `,
-        img : "url"
+        title: `Spring Security 도입 및 Filter Chain 설정 고급화`,
+        description: `테스트 입니다.`
+      },
+      {
+        title: `가독성과 유지보수를 위한 클린 아키텍처 및 CQRS 적용`,
+        description: `테스트 입니다.`
+      },
+      {
+        title: `도메인 로직 보호를 위한 도메인 중심 설계 적용`,
+        description: `테스트 입니다.`
       }
     ]
   },
@@ -70,6 +77,39 @@ export const PROJECTS = [
     ]
   },
   {
+    id: 3,
+    title: "EEOS",
+    semiTitle: "동아리 행사 관리 시스템",
+    detail: `Econovation Event Operating System 의 약자로
+            에코노베이션 동아리의 행사 관리 및 출석 체크 시스템입니다.`,
+    imageSrc: "/static/project/eeos.svg",
+    year: "2024.03 ~ NOW",
+    role: "back-end developer",
+    description: `1. 짧은 기간 내 코드베이스를 빠르게 이해하여 프로젝트 전반 구조를 파악하여 업무 적응
+                  2. Redis Delayed Queue를 활용한 출석체크 예약기능 구현
+                  3. Tree 구조를 활용한, 댓글/답글 구조 모델링`,
+    viewLink: "https://www.eeos.econovation.kr",    // TODO: 실제 프로젝트 URL로 교체
+    githubLink: "https://github.com/JNU-econovation/EEOS-BE",  // TODO: 실제 GitHub 레포로 교체
+    detailDescription: [
+      {
+        title: `짧은 기간 내 코드베이스를 빠르게 이해하여 프로젝트 전반 구조를 파악하여 업무 적응`,
+        description: `테스트 입니다.`
+      },
+      {
+        title: `Redis Delayed Queue를 활용한 출석체크 예약기능 구현`,
+        description: `Redis Sorted Set 자료구조를 사용하여, 각 행사가 종료되는 Timestamp 값을 Score로 사용하였습니다.
+                      
+                      다른 도메인에서도 특정 Task를 예약할 수 있도록 Delayed Queue 클래스를 정의하여 추상화 하였습니다.`,
+        img : "url"
+      },
+      {
+        title: `Tree 구조를 활용한, 댓글/답글 구조 모델링`,
+        description: `행사 조회 페이지에서, 댓글과 `,
+        img : "url"
+      }
+    ]
+  },
+  {
     id: 4,
     title: "ABAS",
     semiTitle: "농업 빅데이터 분석 웹 사이트",
@@ -90,46 +130,6 @@ export const PROJECTS = [
       },
       {
         title: `구아바 캐시를 활용하여 효율적인 메모리 사용의 인증번호 관리 기능 구현`,
-        description: `테스트 입니다.`
-      }
-    ]
-  },
-  {
-    id: 1,
-    title: "Whoz-In",
-    semiTitle: "wi-fi 기반 동아리방 재실 확인 시스템",
-    detail: `Wi-Fi 기술을 기반으로 하는
-            동아리방 재실 인원 현황 확인 서비스 입니다.
-    `,
-    imageSrc: "/static/project/whozIn.svg",
-    year: "2024.09 ~ 2025.03",
-    role: "back-end developer",
-    description: `1. mDNS, ARP 프로토콜을 활용한 동아리방 재실 현황 알고리즘 설계 및 구현
-                  2. Let's Encrypt를 활용한 SSL 적용으로 인한 AWS 서버 비용 감축
-                  3. Spring Security 도입 및 Filter Chain 설정 고급화
-                  4. 가독성과 유지보수를 위한 클린 아키텍처 및 CQRS 적용
-                  5. 도메인 로직 보호를 위한 도메인 중심 설계 적용`,
-    viewLink: "#",
-    githubLink: "https://github.com/JNU-econovation/Whoz-In-BE",
-    detailDescription: [
-      {
-        title: `mDNS, ARP 프로토콜을 활용한 동아리방 재실 현황 알고리즘 설계 및 구현`,
-        description: `테스트 입니다.`
-      },
-      {
-        title: `Let's Encrypt를 활용한 SSL 적용으로 인한 AWS 서버 비용 감축`,
-        description: `테스트 입니다.`
-      },
-      {
-        title: `Spring Security 도입 및 Filter Chain 설정 고급화`,
-        description: `테스트 입니다.`
-      },
-      {
-        title: `가독성과 유지보수를 위한 클린 아키텍처 및 CQRS 적용`,
-        description: `테스트 입니다.`
-      },
-      {
-        title: `도메인 로직 보호를 위한 도메인 중심 설계 적용`,
         description: `테스트 입니다.`
       }
     ]
