f0bbb4e8a8ca0185753f4bc45c12423259ac9e15
Bellmin <whdals011203@naver.com>
2025-09-01 17:39:09 +0900

    refactor: project 순서 변경

--- v3.1/src/portfolio/project/constants/projects.js
+++ v3.1/src/portfolio/project/constants/projects.js
@@ -3,18 +3,21 @@
 export const PROJECTS = [
   {
     id: 1,
-    title: "EEOS",
-    semiTitle: "동아리 행사 관리 시스템",
-    detail: `Econovation Event Operating System 의 약자로
-            에코노베이션 동아리의 행사 관리 및 출석 체크 시스템입니다.`,
-    imageSrc: "/static/project/eeos.svg",
-    year: "2024.03 ~ NOW",
+    title: "Whoz-In",
+    semiTitle: "wi-fi 기반 동아리방 재실 확인 시스템",
+    detail: `Wi-Fi 기술을 기반으로 하는
+            동아리방 재실 인원 현황 확인 서비스 입니다.
+    `,
+    imageSrc: "/static/project/whozIn.svg",
+    year: "2024.09 ~ 2025.03",
     role: "back-end developer",
-    description: `1. 짧은 기간 내 코드베이스를 빠르게 이해하여 프로젝트 전반 구조를 파악하여 업무 적응
-                  2. Redis Delayed Queue를 활용한 출석체크 예약기능 구현
-                  3. Tree 구조를 활용한, 댓글/답글 구조 모델링`,
-    viewLink: "https://www.eeos.econovation.kr",    // TODO: 실제 프로젝트 URL로 교체
-    githubLink: "https://github.com/JNU-econovation/EEOS-BE"  // TODO: 실제 GitHub 레포로 교체
+    description: `1. mDNS, ARP 프로토콜을 활용한 동아리방 재실 현황 알고리즘 설계 및 구현
+                  2. Let's Encrypt를 활용한 SSL 적용으로 인한 AWS 서버 비용 감축
+                  3. Spring Security 도입 및 Filter Chain 설정 고급화
+                  4. 가독성과 유지보수를 위한 클린 아키텍처 및 CQRS 적용
+                  5. 도메인 로직 보호를 위한 도메인 중심 설계 적용`,
+    viewLink: "#",
+    githubLink: "https://github.com/JNU-econovation/Whoz-In-BE"
   },
   {
     id: 2,
@@ -34,6 +37,21 @@ export const PROJECTS = [
   },
   {
     id: 3,
+    title: "EEOS",
+    semiTitle: "동아리 행사 관리 시스템",
+    detail: `Econovation Event Operating System 의 약자로
+            에코노베이션 동아리의 행사 관리 및 출석 체크 시스템입니다.`,
+    imageSrc: "/static/project/eeos.svg",
+    year: "2024.03 ~ NOW",
+    role: "back-end developer",
+    description: `1. 짧은 기간 내 코드베이스를 빠르게 이해하여 프로젝트 전반 구조를 파악하여 업무 적응
+                  2. Redis Delayed Queue를 활용한 출석체크 예약기능 구현
+                  3. Tree 구조를 활용한, 댓글/답글 구조 모델링`,
+    viewLink: "https://www.eeos.econovation.kr",    // TODO: 실제 프로젝트 URL로 교체
+    githubLink: "https://github.com/JNU-econovation/EEOS-BE"  // TODO: 실제 GitHub 레포로 교체
+  },
+  {
+    id: 4,
     title: "ABAS",
     semiTitle: "농업 빅데이터 분석 웹 사이트",
     detail: `농업에 대한 빅데이터의
@@ -46,23 +64,5 @@ export const PROJECTS = [
                   2. 구아바 캐시를 활용하여 효율적인 메모리 사용의 인증번호 관리 기능 구현`,
     viewLink: "https://fe.dev.abas.life",
     githubLink: "#"
-  },
-  {
-    id: 4,
-    title: "Whoz-In",
-    semiTitle: "wi-fi 기반 동아리방 재실 확인 시스템",
-    detail: `Wi-Fi 기술을 기반으로 하는
-            동아리방 재실 인원 현황 확인 서비스 입니다.
-    `,
-    imageSrc: "/static/project/whozIn.svg",
-    year: "2024.09 ~ 2025.03",
-    role: "back-end developer",
-    description: `1. mDNS, ARP 프로토콜을 활용한 동아리방 재실 현황 알고리즘 설계 및 구현
-                  2. Let's Encrypt를 활용한 SSL 적용으로 인한 AWS 서버 비용 감축
-                  3. Spring Security 도입 및 Filter Chain 설정 고급화
-                  4. 가독성과 유지보수를 위한 클린 아키텍처 및 CQRS 적용
-                  5. 도메인 로직 보호를 위한 도메인 중심 설계 적용`,
-    viewLink: "#",
-    githubLink: "https://github.com/JNU-econovation/Whoz-In-BE"
   }
 ];
