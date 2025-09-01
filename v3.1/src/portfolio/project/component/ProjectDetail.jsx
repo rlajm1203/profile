289a18178bcd88f8865106fb63932fcc5e20445f
Bellmin <whdals011203@naver.com>
2025-09-01 16:48:40 +0900

    refactor: Proejct 컨테이너 width 수정

--- v3.1/src/portfolio/project/component/ProjectDetail.jsx
+++ v3.1/src/portfolio/project/component/ProjectDetail.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import ReactMarkDown from 'react-markdown'
 import ProjectHeader from './header/ProjectHeader'
 import ProjectName from './body/ProjectName'
+
 
 const ProjectDetail = ({ project, idx }) => {
 
@@ -11,11 +13,19 @@ const ProjectDetail = ({ project, idx }) => {
   return (
     <div className='container !flex !flex-col justify-between gap-[3dvw]'>
       <ProjectHeader project={project} />
-      <div className='flex flex-col gap-[1dvw]'>
-        <div className="project-name !text-[1.5dvw] !font-light">{updatedProject.title}</div>
-        <div className='!text-[1dvw] !font-light !font-[Manrope] whitespace-pre-line'>{detail[idx].description}</div>
+      <div className='flex flex-row gap-[1dvw]'>
+        <div className='flex flex-col gap-[1dvw]'>
+          <div className="project-name !text-[1.5dvw] !font-light">{updatedProject.title}</div>
+          <div className='!text-[1dvw] !font-light !font-[Manrope] whitespace-pre-line'>
+            {detail[idx].description}
+          </div>
+        </div>
+        <div>
+          {detail[idx].imageSrc ?
+            <img className='w-[50dvw]' src={`${import.meta.env.BASE_URL}${detail[idx].imageSrc}`}></img>
+            : ""}
+        </div>
       </div>
-      {detail[idx].img ? <div className='font-light'>img 있음</div> : ""}
     </div>
   )
 }
--- v3.1/src/portfolio/project/constants/projects.js
+++ v3.1/src/portfolio/project/constants/projects.js
@@ -22,15 +22,39 @@ export const PROJECTS = [
     detailDescription: [
       {
         title: `mDNS, ARP 프로토콜을 활용한 동아리방 재실 현황 알고리즘 설계 및 구현`,
-        description: `테스트 입니다.`
-      },
-      {
-        title: `Let's Encrypt를 활용한 SSL 적용으로 인한 AWS 서버 비용 감축`,
-        description: `테스트 입니다.`
+        description:`
+        문제 1 : Wifi 기반 재실 탐지를 위해, 사용자와 기기 정보 매핑이 필요한 문제.
+
+        해결 1
+        1. WireShark의 CLI 버전인 Tshark 프로세스를, ProcessBuilder를 사용하여 자식 프로세스로 생성
+        2. tShark 프로세스는 ARP, mDNS 프로토콜 패킷의 IP,MAC 주소를 추출하여 출력 스트림으로 전달
+        2. 애플리케이션에서 Non Blocking BufferedReader 와 연결하여, tShark 프로세스의 출력 스트림을 통해 데이터를 수집
+        3. 데이터가 일정량 이상 쌓일 경우, 데이터베이스에 저장
+        4. 이후 사용자가 자신의 IP와 함께 기기등록 요청을 보내면, 수집된 MAC 주소를 조회하여, MAC 주소와 MemberID 매핑
+        
+
+        문제 2 : 기기 등록이 완료된 사용자에 대해서, 실시간으로 재실 확인 수단이 필요한 문제.
+
+        해결 2
+        1. 네트워크 랜카드의 Monitor 모드 적용
+        2. ProcessBuilder를 사용하여, tShark가 Monitor 모드의 네트워크 랜카드로, 목적지 주소에 상관 없이 공기 중에 떠다니는 Wifi 패킷 수집하도록 설정
+        3. 수집된 Monitor 모드 패킷을 불러온 후, 현재 사용자의 기기와 일치하는 MAC 주소가 존재한다면 해당 사용자를 재실 처리
+        4. 수집된 Monitor 모드 패킷을 불러온 후, 사용자의 MAC 주소가 존재하지 않는다면 해당 MAC 주소가 10분 이상 접속을 하지 않았을 경우 퇴실 처리`,
+        imageSrc : "/static/project/whozin-device-register.png"
       },
       {
         title: `Spring Security 도입 및 Filter Chain 설정 고급화`,
         description: `테스트 입니다.`
+      },
+      {
+        title: `Let's Encrypt를 활용한 SSL 적용으로 인한 AWS 서버 비용 감축`,
+        description: `
+        문제 : AWS Application LoadBalancer를 사용한 SSL 인증서 적용 시, 많은 비용 청구 문제
+
+        해결
+        1. Let's Encrypt를 사용한 Web Root 방식의 SSL 인증서 설치
+        2. 애플리케이션 서버 앞단에, nginx 프록시를 배치하여 SSL 적용
+        `
       },
       {
         title: `가독성과 유지보수를 위한 클린 아키텍처 및 CQRS 적용`,
